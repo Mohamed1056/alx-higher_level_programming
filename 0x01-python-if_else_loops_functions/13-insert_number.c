8bdb7868d2d489beb1c8262f71c3e12db2150a43
Mohamed Gamal <mohamed@gmail.com>
2023-10-03 16:12:53 -0700

    adding the 13th file

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -28,6 +28,6 @@ listint_t *insert_node(listint_t **head, int number)
 	while (node && node->next && node->next->n < number)
 		node = node->next;
 	new->next = node->next;
-	nod->next = new;
+	node->next = new;
 	return (new);
 }
