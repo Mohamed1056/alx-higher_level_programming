cba9715ba9280e4c82cb69fb8052abf3f0e76e35
Mohamed Gamal <mohamed@gmail.com>
2023-10-03 16:05:24 -0700

    adding the 13th file

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS-H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: takes the intger
+ * @next: pointer to the next point
+*/
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}listint_t;
+
+listint_t *insert_node(listint_t **head, int number);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+#endif
