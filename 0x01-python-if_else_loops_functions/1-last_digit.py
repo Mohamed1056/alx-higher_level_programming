f3bd2eaa0387e972f0c09b42bea565cf565f2e48
Mohamed Gamal <mohamed@gmail.com>
2023-10-03 13:42:30 -0700

    checking the modulus of number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,10 @@ modnum = abs(number) % 10
 if (number < 0):
     modnum = -modnum
 if (modnum > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, modnum))
+    print("Last digit of {} is {} and 
+            is greater than 5".format(number, modnum))
 elif (modnum == 0):
     print("Last digit of {} is {} and is 0".format(number, modnum))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, modnum))
+    print("Last digit of {} is {} and 
+    is less than 6 and not 0".format(number, modnum))
