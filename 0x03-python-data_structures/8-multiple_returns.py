3443367d3afa34ba32d6d1e8e639df303e04fe63
Mohamed Gamal <mohamed@gmail.com>
2023-10-08 16:35:33 -0700

    adding the nineth file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0] if sentence else "None"
-    return(length, first_char)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
