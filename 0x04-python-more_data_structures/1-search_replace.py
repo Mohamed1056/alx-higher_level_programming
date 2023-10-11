e107b01ddf742b5dfbe1b8f6c381ac2ee420b9c1
Mohamed Gamal <mohamed@gmail.com>
2023-10-11 11:35:54 -0700

    adding the 1st file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def search_replace(my_list, search, replace):
+def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda(x : x ** 2), matrix[i])
