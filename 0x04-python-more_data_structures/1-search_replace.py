d3f4fdbeb6af440fb2d721e2a2b4f1e5c3f20715
Mohamed Gamal <mohamed@gmail.com>
2023-10-11 11:31:58 -0700

    adding the 1st file

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_matrix = matrix.copy()
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda(x : x ** 2), matrix[i])
+    return (new_matrix)
