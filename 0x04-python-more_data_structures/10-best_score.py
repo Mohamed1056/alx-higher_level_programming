31904fcb64846aba7fb929d3e332eb5fac30af31
Mohamed Gamal <mohamed@gmail.com>
2023-10-12 09:50:05 -0700

    adding the 11th file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    new_list = list(a_dictionary.values())
-    c = 0
 
-    for i in new_list:
-         if c <= i:
-            c = i
-    return (c)
+    return (max(a_dictionary, key=a_dictionary.get))
