ba22d96f98eb6e6c2b02f4a168e077df06ec98a3
Mohamed Gamal <mohamed@gmail.com>
2023-10-12 05:54:17 -0700

    adding the 3rd file

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    new_list = set(my_list)
+    uniq_list = set(my_list)
     num = 0
-    for i in new_list:
-      num += i
-    return num
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
