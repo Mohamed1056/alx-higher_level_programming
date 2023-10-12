db7b93f5323592dc1fd47ee5678b9d333f155ec9
Mohamed Gamal <mohamed@gmail.com>
2023-10-12 10:43:07 -0700

    adding the 14th file

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    num , summ = 0, 0
     if not my_list:
         return 0
+    num = 0
+    summ = 0
     for i in range(len(my_list)):
         mul = my_list[i][0] * my_list[i][1]
         fum = my_list[i][1]
