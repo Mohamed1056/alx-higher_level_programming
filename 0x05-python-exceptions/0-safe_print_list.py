b574845ef72bf6bf450eda5edb6c5330a0ed9e59
Mohamed Gamal <mohamed@gmail.com>
2023-10-23 11:47:30 -0700

    adding the first files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-
+        while i is not x:
+            print(my_list[i], end='')
+            i += 1
     except IndexError:
-        if (x > len(my_list)):
-            x = len(my_list)
+        None
     print()
-    return (x)
+    return (i)
