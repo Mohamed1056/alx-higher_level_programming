50924b4d090a09e6aac0f66955c1c3b6afb3a4ff
Mohamed Gamal <mohamed@gmail.com>
2023-10-25 15:22:55 -0700

    adding the first file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Define a class Square"""
+"""Square module"""
 
 class Square:
-    """Define the class square."""
+    """Define a square."""
 
     def __init__(self, size):
-        """Initialize a new Square.
+        """Constructor.
 
         Args:
             size(int):The size of the new square.
