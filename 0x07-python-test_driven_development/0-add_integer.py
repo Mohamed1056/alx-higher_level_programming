0eff5d9a99b2e1b24580cd55544f3facf11fb310
Mohamed Gamal <mohamed@gmail.com>
2023-10-30 14:12:08 -0700

    fixing the first file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for add_integer method."""
 
+
 def add_integer(a, b=98):
     """Adds two integers.
 
@@ -20,6 +21,7 @@ def add_integer(a, b=98):
     if type(b) not in (int, float):
         raise TypeError('b must be an integer')
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
