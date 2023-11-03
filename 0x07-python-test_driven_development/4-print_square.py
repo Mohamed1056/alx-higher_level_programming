8f72c8cbb779875276aed0cfdb9a36fc81323c6f
Mohamed Gamal <mohamed@gmail.com>
2023-11-03 04:36:40 -0700

    fixing the fourth file

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,6 +14,8 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
     print((("#" * size + "\n") * size), end="")
 
 
