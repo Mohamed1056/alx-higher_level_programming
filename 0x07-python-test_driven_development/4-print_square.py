ffa5fc6643b8ac07e61428767033d0b34f0e5779
Mohamed Gamal <mohamed@gmail.com>
2023-11-03 04:39:11 -0700

    fixing the fourth file

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+"""Module for print_square method."""
 
 
 def print_square(size):
-    """Method for printing a suqre.
-    Args:
-        size: the int size of the square
+    """Method for printing a square with # characters.
 
-    Rqises:
+    Args:
+        size: The int size of the square's side.
+
+    Raises:
         TypeError: If size is not an int.
         ValueError: If size is < 0.
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
+
     print((("#" * size + "\n") * size), end="")
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/4-print_square.txt")
