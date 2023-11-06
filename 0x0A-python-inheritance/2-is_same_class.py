b4b6806dc60f0ec0e06e96aa0c4493177f118fc2
Mohamed Gamal <mohamed@gmail.com>
2023-11-06 14:09:29 -0800

    adding the third file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module for is_same_class method'''
+
+
+def is_same_class(obj, a_class):
+    '''Determines if an object is exactly having the same type'''
+    return type(obj) == a_class
