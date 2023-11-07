8990c0ca1973f1447392b2cec36c1f317460b73a
Mohamed Gamal <mohamed@gmail.com>
2023-11-07 15:13:49 -0800

    adding the 13th file

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,6 +12,6 @@ def pascal_triangle(n):
         tmp = [1]
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
-        tmp.append[1]
+        tmp.append(1)
         triangles.append(tmp)
     return triangles
