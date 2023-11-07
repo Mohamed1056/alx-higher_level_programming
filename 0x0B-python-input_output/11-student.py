d18a35932f0579ae4e05347bbb0b00b5d5690edb
Mohamed Gamal <mohamed@gmail.com>
2023-11-07 15:03:01 -0800

    adding the 12th file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''Module for class method.'''
+
+
+class Student:
+    """representaion of a student class."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation."""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        """returns a json."""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
