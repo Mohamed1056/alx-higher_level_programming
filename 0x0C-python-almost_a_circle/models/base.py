5ad20b092c352e1b348738cadeb5e6135dc3f8b1
Mohamed Gamal <mohamed@gmail.com>
2023-11-12 04:29:35 -0800

    adding all the files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Module for Base class'''
+
+
+class Base:
+    '''representaion of class Base'''
+    __nb_objects = 0
+    def __init__(self, id=None):
+        '''constructor.'''
+        if not id:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+        
