50f569b6cf5cfbf389e56332084943c7dfd23c58
Mohamed Gamal <mohamed@gmail.com>
2023-11-12 04:32:47 -0800

    adding all the files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,11 +5,11 @@
 class Base:
     '''representaion of class Base'''
     __nb_objects = 0
+    
     def __init__(self, id=None):
         '''constructor.'''
         if not id:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
-            self.id = id
-        
+            self.id = id    
