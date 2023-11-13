f453eb4af152895653d9997e541925f7d6583140
Mohamed Gamal <mohamed@gmail.com>
2023-11-13 01:41:13 -0800

    updating square module

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,10 +9,19 @@ class Square(Rectangle):
     '''representaion of the class.'''
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor.'''
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         '''represent string for the user.'''
         return "[{}] ({}) {}/{} - {}".\
             format(type(self).__name__, self.id, self.x, self.y, self.size)
+
+    @property
+    def size(self):
+        '''Size of the square.'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
