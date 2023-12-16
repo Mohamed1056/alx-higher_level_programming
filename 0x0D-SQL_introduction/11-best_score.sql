4028c1570b4822c85e169977cfe1f1ad58656f2d
Mohamed Gamal <mg8033667@gmial.com>
2023-12-16 03:05:47 -0800

    adding 11-best_score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Script to print the scoreand the name
 -- The score must be bigger than or equal 10
-SELECT score, name FROM second_table WHERE score>=10 AND ORDER BY score DSC;
+SELECT score, name FROM second_table WHERE score>=10 ORDER BY score DESC;
