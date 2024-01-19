b17ff5466553198276fc2cfe2ca43b13f240a5e7
Mohamed Gamal <mg8033667@gmail.com>
2024-01-19 13:07:20 -0800

    another sqlalchemy file3

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""file to search the name of the state."""
+
+
+from base_model import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexEerror:
+        pprint("Not found")
