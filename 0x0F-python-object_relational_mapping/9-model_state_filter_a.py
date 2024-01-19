c1ffb6c80d18d0be59c23b73c50fc9ef02fb0a51
Mohamed Gamal <mg8033667@gmail.com>
2024-01-19 12:55:55 -0800

    another sqlalchemy file2

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""finding a country that has letter a."""
+""" prints the first State object from the database hbtn_0e_6_usa."""
 
-
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
@@ -14,5 +13,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like(%a%)):
-        print(insatnce.id, instance.name, sep=": ")
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
